161d09ee940e232eae4e35d45c2d50b5a6d73da5
Sérgio Filippo <m4n0x.br0@gmail.com>
2025-05-31 13:45:46 -0300

    exercicios hora do sistema

---
+++ src/exercicios/HoradoSistema.java
@@ -0,0 +1,11 @@
+package exercicios;
+
+import java.util.Date;
+
+public class HoradoSistema {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.print("A hora do sistema: ");
+        System.out.print(relogio.toString());
+    }
+}
