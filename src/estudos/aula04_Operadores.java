b46ebb9bed2e77203069306b40bb11508f7903f2
Sérgio Filippo <m4n0x.br0@gmail.com>
2025-06-01 13:12:26 -0300

    operadores

--- src/estudos/aula04_Operadores.java
+++ src/estudos/aula04_Operadores.java
@@ -13,8 +13,29 @@ public class aula04_Operadores {
         System.out.println(resto);
 
         // < > <= >= == !=
-        boolean isDezMaiorQueVinte = 10>20;
+        boolean isDezMaiorQueVinte = 10 > 20;
         System.out.println("isDezMaiorQueVinte: " + isDezMaiorQueVinte);
+
+        // && (and) - || (or) - !
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystarionCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println("isPlaystarionCincoCompravel " + isPlaystarionCincoCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        System.out.println(bonus);
+
+
 
     }
 }
---
+++ src/estudos/aula05_EstruturasCondicionais.java
@@ -0,0 +1,4 @@
+package estudos;
+
+public class aula05_EstruturasCondicionais {
+}
