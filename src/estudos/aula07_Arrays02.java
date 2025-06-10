776e27f6fdfeed257df619747d249e7bec781487
Sérgio Filippo <m4n0x.br0@gmail.com>
2025-06-10 19:40:37 -0300

    arrays

---
+++ src/estudos/aula07_Arrays02.java
@@ -0,0 +1,19 @@
+package estudos;
+
+public class aula07_Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float e double = 0
+        // char '\u0000'
+        // boolean false
+        // String null
+        String [] nomes = new String[3];
+        nomes [0] = "Goku";
+        nomes [1] = "Kurosaki";
+        nomes [2] = "Luffy";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+
+    }
+}
