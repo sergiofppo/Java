3424525aa222d9d925fe77a271cc05f74cd173a4
Sérgio Filippo <m4n0x.br0@gmail.com>
2025-06-13 11:16:49 -0300

    parametros

--- src/estudos/javacore/Bintroducao_metodos/dominio/Calculadora.java
+++ src/estudos/javacore/Bintroducao_metodos/dominio/Calculadora.java
@@ -4,7 +4,12 @@ public class Calculadora {
     public void somaDoisNumeros () {
         System.out.println(10+10);
     }
+
     public void subtraiDoisNumeros() {
         System.out.println(21-2);
+    }
+
+    public void multiplicaDoisNumeros(int num1, int num2) {
+        System.out.println(num1 * num2);
     }
 }
