b40d22bbafa18fd7405c78954133b846e0306a44
Sérgio Filippo <m4n0x.br0@gmail.com>
2025-09-08 12:04:18 -0300

    Add ex04 exercise and minor cleanup in ex03
    Added a new exercise (ex04.java) to count even and odd numbers in a 3x3 matrix input by the user. Also performed minor code cleanup and comment adjustments in ex03.java for better readability.

--- src/estudos/ArraysMutidimendionais/exercicios/ex03.java
+++ src/estudos/ArraysMutidimendionais/exercicios/ex03.java
@@ -7,7 +7,8 @@ public class ex03 {
     public static void main(String[] args) {
         int[][] numeros = new int[10][10];
         Random numerosAleatorios = new Random();
-        // Gera a matriz 10x10 com numeros aleatorios
+
+        // gera a matriz 10x10 com numeros aleatorios
         for (int i = 0; i < numeros.length; i++) {
             for (int j = 0; j < numeros[i].length; j++) {
                 numeros[i][j] = numerosAleatorios.nextInt(10);
@@ -19,14 +20,13 @@ public class ex03 {
         int maiorColuna = Integer.MIN_VALUE;
         int menorColuna = Integer.MAX_VALUE;
 
-        //
         for (int j = 0; j < numeros[5].length; j++) {
-               if (numeros[5][j] > maiorLinha) {
-                   maiorLinha = numeros[5][j];
-               }
-               if (numeros[5][j] < menorLinha) {
+            if (numeros[5][j] > maiorLinha) {
+                maiorLinha = numeros[5][j];
+            }
+            if (numeros[5][j] < menorLinha) {
                    menorLinha = numeros[5][j];
-               }
+            }
         }
 
         for (int i = 0; i < numeros.length; i++) {
---
+++ src/estudos/ArraysMutidimendionais/exercicios/ex04.java
@@ -0,0 +1,37 @@
+package estudos.ArraysMutidimendionais.exercicios;
+
+import java.util.Scanner;
+
+public class ex04 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[][] matriz = new int[3][3];
+        int qntdPares = 0;
+        int qntdImpares = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print("Escreva o valor dos numeros da matriz: ");
+                matriz[i][j] = scan.nextInt();
+            }
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] % 2 ==0) {
+                    qntdPares++;
+                } else {
+                    qntdImpares++;
+                }
+            }
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j]+ " ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println(" ");
+        System.out.println("\nQuantidade de numeros Pares: " + qntdPares);
+        System.out.print("Quantidade de numeros Impares: " + qntdImpares);
+    }
+}
