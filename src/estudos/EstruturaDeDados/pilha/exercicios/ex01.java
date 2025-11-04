096acc79827c1fe8726756d2808983f529e438b7
Sérgio Filippo <m4n0x.br0@gmail.com>
2025-11-04 17:21:39 -0300

    Add stack exercise to pilha exercicios
    Introduces ex01.java, a program that reads 10 numbers, pushes even numbers onto a stack, and pops the stack for odd numbers. At the end, any remaining items in the stack are popped and displayed.

---
+++ src/estudos/EstruturaDeDados/pilha/exercicios/ex01.java
@@ -0,0 +1,37 @@
+package estudos.EstruturaDeDados.pilha.exercicios;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ex01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+1 + ". Digite um numero: ");
+            int numero = scan.nextInt();
+            if (numero % 2 == 0) {
+                System.out.println("Numero par! Emplilhando..." + stack.push(numero));
+            } else {
+                if (stack.isEmpty()) {
+                    System.out.println("A pilha esta vazia!");
+                } else {
+                    System.out.println("Numero Impar! Desimpilhando..." + stack.pop());
+                }
+            }
+            System.out.println("Pilha agora: " + stack);
+            System.out.println("--------------------");
+        }
+        System.out.println("\n--- FIM DA LEITURA ---");
+        if (stack.isEmpty()) {
+            System.out.println("A pilha esta vazia!");
+        } else {
+            System.out.println("Desempilhando todos os itens que sobraram:");
+            while (!stack.isEmpty()) {
+                System.out.println("Saiu: " + stack.pop());
+            }
+        }
+        scan.close();
+    }
+}
