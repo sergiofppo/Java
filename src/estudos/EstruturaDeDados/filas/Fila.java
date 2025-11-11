e0658d37d226982486752abaefe2496c4c51ec70
Sérgio Filippo <m4n0x.br0@gmail.com>
2025-11-11 14:27:03 -0300

    Add basic Fila class and test for queue structure
    Introduces the Fila class extending EstruturaEstatica to represent a generic queue. Also adds teste13.java to test basic queue operations like isEmpty and tamanho.

---
+++ src/estudos/EstruturaDeDados/filas/Fila.java
@@ -0,0 +1,15 @@
+package estudos.EstruturaDeDados.filas;
+
+import estudos.EstruturaDeDados.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+
+    public Fila() {
+        super();
+    }
+
+    public Fila(int capacidade) {
+        super(capacidade);
+    }
+
+}
---
+++ src/estudos/EstruturaDeDados/testes/teste13.java
@@ -0,0 +1,12 @@
+package estudos.EstruturaDeDados.testes;
+
+import estudos.EstruturaDeDados.filas.Fila;
+
+public class teste13 {
+    public static void main(String[] args) {
+        Fila<Integer> fila = new Fila<>();
+
+        System.out.println(fila.isEmpty());
+        System.out.println(fila.tamanho());
+    }
+}
